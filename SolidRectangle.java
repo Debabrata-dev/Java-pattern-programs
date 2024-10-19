e1ef150a2150684248970bd692bb88eb1d0125c1
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-10-19 22:28:49 +0530

    Add files via upload
    JAVA PATTERS QUESTION WITH ANS

---
+++ SolidRectangle.java
@@ -0,0 +1,12 @@
+public class SolidRectangle {
+public static void main(String[] args) {
+    //nested loops
+    for(int i=1;i<5;i++){
+      //inner loops
+      for(int j=0;j<5;j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+}  
+}
